d71bf20d41aa463ff4f5254f4feaca8415621a2f
okobb <okobrosli2@gmail.com>
2025-11-08 13:46:01 +0200

    Added the AI code checker response API.

--- backend/create_review.php
+++ backend/create_review.php
@@ -6,7 +6,7 @@ $api_key = "sk-proj-Ph-LNTncMkq6zIz7suUzZDfHxMi_-EzbYXlj2ghT3sBacccFgae5APiixeSr
 $url = 'https://api.openai.com/v1/chat/completions';
 
 
-$user_prompt = "Write me a simple python code to add 2 numbers.";
+$user_prompt = "I need you to check this code for me for any possible mistakes, give me the response in JSON format according to the following instructions. 1. Severity : high,medium,or low. 2. The file name (or nothing if its not a file). 3. A short identifier of the issue. 4. A suggestion to fix the code .The reply should only contain the JSON code and no other data inside it.The code is : def greet_and_age(name, age)   print('Hello, ' + name) next_age = age + 1s print('Next year you will be: ' + next_age) greet_and_age('Jesse', '29') ";
 
 $data = [
         'model'    => 'gpt-4o',
@@ -24,8 +24,9 @@ $options = [
 ];
 
 $context  = stream_context_create($options);
-$response = file_get_contents('https://api.openai.com/v1/chat/completions', false, $context);
+$response = file_get_contents($url, false, $context);
 $response = json_decode($response);
 $response = $response->choices[0]->message->content;
+
 echo $response;
 ?>
