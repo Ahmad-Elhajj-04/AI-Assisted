bf9368e726444e9b9621d5395ef1ecea5b6f184e
Ahmad AlHaj <ahmadhaj.2004aa@gmail.com>
2025-11-09 18:12:35 +0200

    adding checkbox and plus adding css and html.

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -7,3 +7,14 @@ document.getElementById('sender').onclick =  async function sendData() {
    const data = response.data;
    console.log(data.reviews);
 }
+function myFunction() {
+const checkBox = document.getElementById("myCheck");
+const text = document.getElementById("text");
+if (checkBox.checked == true) {
+    text.style.display = "block";
+}
+else {
+    text.style.display = "none";
+  }
+}
+
