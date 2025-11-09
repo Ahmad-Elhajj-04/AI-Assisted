83a066512f35215a0701956dbd5352d741d0351d
okobb <okobrosli2@gmail.com>
2025-11-09 11:58:56 +0200

    Fixed axios function to send and recieve data.

--- backend/add_code.php
+++ backend/add_code.php
@@ -1,51 +1,19 @@
 <?php
 
-include("connection/connection.php");
-
-$severity_check = ["high", "medium", "low"];
-
-if(isset($_POST["severity"]) && in_array($_POST["severity"],$severity_check)){
-    $severity = $_POST["severity"];
-}else{
-    $response = [];
-
-    $response["success"] = false;
-    $response["error"] = "Severity field has the wrong input.";
-    echo json_encode($response);
-    return;
-}
-
-if(isset($_POST["file"]) && $_POST["file"] != "" && (str_contains($_POST["file"],"."))){
-    $file = $_POST["file"];
-}else{
-    $response = [];
-    $response["success"] = false;
-    $response["error"] = "File field has the wrong input.";
-    echo json_encode($response);
-    return;
-}
-
-if(isset($_POST["issue"]) && $_POST["issue"] != ''){
-    $issue = $_POST["issue"];
-}else{
-    $response = [];
-    $response["success"] = false;
-    $response["error"] = "Issue field has the wrong input.";
-    echo json_encode($response);
-    return;
-}
-
-if(isset($_POST["suggestion"]) && $_POST["suggestion"] != ""){
-    $suggestion = $_POST["suggestion"];
-}else{
-    $response = [];
-    $response["success"] = false;
-    $response["error"] = "Suggestion field has the wrong input.";
-    echo json_encode($response);
-    return;
-}
-
+$data = json_decode(file_get_contents("php://input"), true);
 $response = [];
+if(isset($data["code"]) && $data["code"] != ""){
+    $response['code'] = $data["code"];
+}else{
+    $response = [];
+    $response["success"] = false;
+    $response["error"] = "Name field is missing";
+    echo json_encode($response);
+    return;
+}
+
+
+
 $response["success"] = true;
 echo json_encode($response);
 ?>
--- frontend/script.js
+++ frontend/script.js
@@ -1,14 +1,16 @@
 // Simple axios example - just the basics
 
-function sendData() {
-    axios.post('http://localhost/AI-Assisted/backend/review.php', {
-        code: "def get_input() return input('Enter: ')",
+document.getElementById('sender').onclick =  async function sendData() {
+    await axios.post('http://localhost/AI-Assisted/backend/add_code.php', {
+        code: document.getElementById('code').value,
         filename: "app.py"
     })
     .then(function (response) {
-        console.log(response.data);
+        console.log(response.data.code);
     })
     .catch(function (error) {
         console.log(error.message);
     });
+
+    
 }
